7a5794e1075291cbe0177e161d88540c87838f61
fupn26 <43246998+fupn26@users.noreply.github.com>
2018-09-19 21:47:44 +0200

    Add files via upload

---
+++ m_to_km_converter.cpp
@@ -0,0 +1,10 @@
+#include "std_lib_facilities.h"
+int main()
+{
+cout<<"++++++++++Miles to kilometer converter++++++++++\n";
+cout<<"mile = ";
+double mile;
+cin>>mile;
+double km = mile*1.609;
+cout<<"kilometer = "<<km<<'\n';
+}
